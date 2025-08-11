960c6eefc140179f471c3a9a167a472145c7900f
masoodraza-dev <masoodr508@gmail.com>
2025-08-12 00:57:31 +0500

    Update Addition.java
    updated

--- Rational_Number_Task/Addition.java
+++ Rational_Number_Task/Addition.java
@@ -1,8 +1,9 @@
-public class Subtract {
-    public static void subtract(FractionOps other1, FractionOps other2) {
-        int numerator = (other1.numer * other2.denom) - (other1.denom * other2.numer);
+public class Addition {
+
+    public static void addition(FractionOps other1, FractionOps other2) {
+        int numerator = (other1.numer * other2.denom) + (other1.denom * other2.numer);
         int denominator = other1.denom * other2.denom;
-        System.out.println("\n SUBTRACTION");
+        System.out.println("\n ADDITION");
         FractionOps.simplify(numerator, denominator);
     }
 
