19d673c58e4a35b81cbf15ad4f46e119e57fddd4
masoodraza-dev <masoodr508@gmail.com>
2025-08-12 00:54:54 +0500

    Create Addition.java
    Addition class

---
+++ Rational_Number_Task/Addition.java
@@ -0,0 +1,9 @@
+public class Subtract {
+    public static void subtract(FractionOps other1, FractionOps other2) {
+        int numerator = (other1.numer * other2.denom) - (other1.denom * other2.numer);
+        int denominator = other1.denom * other2.denom;
+        System.out.println("\n SUBTRACTION");
+        FractionOps.simplify(numerator, denominator);
+    }
+
+}
