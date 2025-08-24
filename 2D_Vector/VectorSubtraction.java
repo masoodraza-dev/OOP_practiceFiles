3771a801838ebe02e5b4bed14677095725d03215
masoodraza-dev <masoodr508@gmail.com>
2025-08-24 23:28:26 +0500

    Create VectorSubtraction.java
    Subtraction class

---
+++ 2D_Vector/VectorSubtraction.java
@@ -0,0 +1,5 @@
+public class VectorSubtraction {
+    public static Vector2D subtract(Vector2D v1, Vector2D v2) {
+        return new Vector2D(v1.getX() - v2.getX(), v1.getY() - v2.getY());
+    }
+}
