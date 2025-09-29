5bfc48ff3126aaaff87f43da215a5a4faf21aea4
masoodraza-dev <masoodr508@gmail.com>
2025-09-30 01:31:51 +0500

    Update and rename StoreSystem.java to Main.java
    ok

---
+++ Money_Change_System/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Cashier cashier = new Cashier();
+        Customer customer = new Customer();
+        customer.payBill(cashier);
+        
+
+        
+
+        
+    }
+}
